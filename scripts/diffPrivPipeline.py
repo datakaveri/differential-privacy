43e222bf57bf71de4d12170f4836868f395f5f91
novoneel-iudx <novoneel.chakraborty@datakaveri.org>
2023-07-09 00:01:24 +0530

    updates to output format to convert to merged dataframe. Changes to clipping mechanism to account for different clipping needs for query2 spatio-temporal

--- scripts/diffPrivPipeline.py
+++ scripts/diffPrivPipeline.py
@@ -148,6 +148,8 @@ def postProcessingSpatioTemporal(dfNoiseQuery1, dfNoiseQuery2, bVarianceQuery1, 
     print('\n################################################################\n')
     print('POSTPROCESSING')
     #postprocessing
+    dfNoiseQuery1.name = 'dfFinalQuery1'
+    dfNoiseQuery2.name = 'dfFinalQuery2'
     dfFinalQuery1 = postmod.postProcessing(dfNoiseQuery1, configDict, genType)
     dfFinalQuery2 = postmod.postProcessing(dfNoiseQuery2, configDict, genType)
     
@@ -165,6 +167,7 @@ def postProcessingSpatioTemporal(dfNoiseQuery1, dfNoiseQuery2, bVarianceQuery1, 
         print("The MAE is: ", maeQuery1)
         print('\n\nFor Query 2: ')
         postmod.signalToNoise(snrAverageQuery2, configDict)
+
         maeQuery2 = stmod.maeCompute(signalQuery2, noiseQuery2)
         print("The MAE is: ", maeQuery2)
 
@@ -178,8 +181,11 @@ def postProcessingSpatioTemporal(dfNoiseQuery1, dfNoiseQuery2, bVarianceQuery1, 
     postmod.cumulativeEpsilon(configDict)
     
     #creating the output files
-    postmod.outputFile(dfFinalQuery1, 'dfNoisyITMS1')
-    postmod.outputFile(dfFinalQuery2, 'dfNoisyITMS2')
+    print('dfFinalQuery1')
+    print(dfFinalQuery1)
+    print('dfFinalQuery2')
+    print(dfFinalQuery2)
+    postmod.outputFile(dfFinalQuery1, dfFinalQuery2)
     
     print('Differentially Private output generated. Please check the pipelineOutput folder.')
     print('\n################################################################\n')
--- scripts/postProcessing.py
+++ scripts/postProcessing.py
@@ -1,21 +1,25 @@
 import numpy as np
+import pandas as pd
 
 def postProcessing(dfNoise, configDict, genType):
     if genType == 'spatio-temporal':
         #postprocessing ITMSQuery1
         globalMaxValue = configDict['globalMaxValue']
         globalMinValue = configDict['globalMinValue']
-        dfFinalITMSQuery1 = dfNoise
-        dfFinalITMSQuery1['queryNoisyOutput'].clip(globalMinValue, globalMaxValue, inplace = True)
-        if configDict['optimized'] == False:
-            dfFinalITMSQuery1.drop(['queryOutput'], axis = 1, inplace = True)        
-        # #postprocessing ITMS Query 2
-        # dfFinalITMSQuery2 = dfNoiseITMSQuery2
-        # dfFinalITMSQuery2['query2NoisyOutput'].clip(0, np.inf, inplace = True)
-        # dfFinalITMSQuery2.drop(['query2Output'], axis = 1, inplace = True)
-        
-        return dfFinalITMSQuery1
-    
+
+        if dfNoise.name == 'dfFinalQuery1':
+            dfFinalITMSQuery1 = dfNoise
+            dfFinalITMSQuery1['queryNoisyOutput'].clip(globalMinValue, globalMaxValue, inplace = True)
+            if configDict['optimized'] == False:
+                dfFinalITMSQuery1.drop(['queryOutput'], axis = 1, inplace = True)
+            return dfFinalITMSQuery1
+        elif dfNoise.name == 'dfFinalQuery2':
+            dfFinalITMSQuery2 = dfNoise
+            dfFinalITMSQuery2['queryNoisyOutput'].clip(0, np.inf, inplace = True)
+            if configDict['optimized'] == False:
+                dfFinalITMSQuery2.drop(['queryOutput'], axis = 1, inplace = True)
+            return dfFinalITMSQuery2
+
     elif genType == 'categorical':
         dfFinal = dfNoise
         dfFinal['roundedNoisyCount'] = dfFinal['noisyCount'].round()
@@ -44,6 +48,33 @@ def cumulativeEpsilon(configDict):
     print('\nYour Cumulative Epsilon for the displayed queries is: ' + str(cumulativeEpsilon))
     return cumulativeEpsilon
 
-def outputFile(dfFinal, dataframeName):
-    dfFinal.to_csv('../pipelineOutput/' + dataframeName + '.csv')
+
+def createNestedJSON(dataframe, parent_col):
+    result = []
+    for _, row in dataframe.iterrows():
+        current = result
+        for i, col in enumerate(dataframe.columns):
+            if col == parent_col:
+                continue
+            if i == len(dataframe.columns) - 1:
+                current.append(row[col])
+            else:
+                value = row[col]
+                if col not in current[-1]:
+                    current[-1][col] = {}
+                current = current[-1][col]
+                if isinstance(value, pd.DataFrame):
+                    current[col] = createNestedJSON(value, parent_col)
+                else:
+                    current[col] = value
+    return result
+
+def outputFile(dfFinalQuery1, dfFinalQuery2):
+    dfFinal = pd.DataFrame()
+    dfFinal['HAT'] = dfFinalQuery1['HAT']
+    dfFinal['query1NoisyOutput'] = dfFinalQuery1['queryNoisyOutput']
+    dfFinal['query2NoisyOutput'] = dfFinalQuery2['queryNoisyOutput']
+    print(dfFinal)
+    createNestedJSON(dfFinal, 'HAT')
+    dfFinal.to_json('../pipelineOutput/' + 'noisyOutput' + '.json')
     return
