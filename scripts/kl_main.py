38beee23ae939ac923ac20641f76cfc5b729b975
Reddy-nk <kailash.nanaluri@gmail.com>
2023-08-30 18:06:27 +0530

    Modified code for some corner cases and added some extra features

--- scripts/kl_main.py
+++ scripts/kl_main.py
@@ -4,11 +4,18 @@ import numpy as np
 import time
 start_time=time.time()
 end_time=0
+global df_copy
+global merged_categories
+merged_categories = []
+
+#Checks both if a partiton is K-anonymized and also L-diverse
 def isKAnonymized(df,partition,sensitive_column, k,l):
     unique_values_count = df.loc[partition, sensitive_column].nunique()
     if (len(partition) < k) or unique_values_count<l:
         return False
+    
     return True
+
 #function for calculating the spans of each column in a partition 
 def get_spans(df, partition,categorical, scale=None ):
     spans = {}
@@ -21,15 +28,11 @@ def get_spans(df, partition,categorical, scale=None ):
             span = span/scale[column]
         spans[column] = span
     return spans
-'''split function takes a dataframe, partition and column and 
-returns two partitions that split the given partition such that
- all rows with values of the column column below the median are in one partition 
- and all rows with values above or equal to the median are in the other.'''
 
-#need  to change the split function such that it creates half half bins every time
+#Spltiing the partitons based on median if spltiing through numerical , else splitting through all categories for all categories
 def split(df, partition, categorical,column):
     dfp = df[column][partition]
-    if column in categorical:
+    if (column in categorical) and categorical != []:
         values = dfp.unique()
         lv = set(values[:len(values)//2])
         rv = set(values[len(values)//2:])
@@ -39,42 +42,43 @@ def split(df, partition, categorical,column):
         dfl = dfp.index[dfp < median]
         dfr = dfp.index[dfp >= median]
         return (dfl, dfr)
+
+
 def partition_dataset(df, feature_columns,categorical,numerical,scale,sensitive_column,k,l_div):
+    global df_copy 
+    global merged_categories
+    df_copy = df.copy()
     #assigning the index of the whole dataframe
     working_partitions = [df.index]
-
-    '''partition_flags = {}
-
-    for partition in working_partitions:
-        partition_flags[partition] = True'''
-
-    spans = get_spans(df[categorical],df.index,categorical, scale)
-    for column, span in sorted(spans.items(), key=lambda x:-x[1]):
-        l=len(working_partitions)
-        unique_categories = df[column].unique()
-        while l>0:
-            partition = working_partitions.pop(0)
-            category_partitions = {category: [] for category in unique_categories}
-            for idx in partition:
-                category = df.at[idx, column]
-                category_partitions[category].append(idx)
-            partition_lists = [partition for partition in category_partitions.values()]
-            flag=0
-            for list in partition_lists:
-                if isKAnonymized(df,list,sensitive_column,k,l_div)==False:
-                    flag=1
-                    break
-            if flag==0:
-                for partition in partition_lists:
-                    working_partitions.append(partition)
-            else:
-                working_partitions.append(partition)
-            l=l-1
-    prev_l=len(working_partitions)
-    while True:
-        spans = get_spans(df[numerical], partition,categorical, scale)
+    
+    if categorical != []:#This categorical list gets updated with possible categories on which K-anonimity is possible
+        spans = get_spans(df[categorical],df.index,categorical, scale)
         for column, span in sorted(spans.items(), key=lambda x:-x[1]):
-            print("column:",column)
+            length=len(working_partitions)
+            unique_categories = df[column].unique()
+            while length>0:
+                partition = working_partitions.pop(0)
+                category_partitions = {category: [] for category in unique_categories}
+                for idx in partition:
+                    category = df.at[idx, column]
+                    category_partitions[category].append(idx)
+                partition_lists = [partition for partition in category_partitions.values()]
+                flag=0
+                for list in partition_lists:
+                    if isKAnonymized(df,list,sensitive_column,k,l_div)==False:
+                        flag=1
+                        break
+                if flag==0:
+                    for partition in partition_lists:
+                        working_partitions.append(partition)
+                else:
+                    working_partitions.append(partition)
+                length=length-1
+    prev_l=len(working_partitions)
+
+    while True:#now splits through numerical columns
+        for column in numerical:
+            print("Splitting through:",column)
             l=len(working_partitions)
             while l>0:
                 partition = working_partitions.pop(0)
@@ -88,7 +92,18 @@ def partition_dataset(df, feature_columns,categorical,numerical,scale,sensitive_
             break
         else:
             prev_l=len(working_partitions) 
-                    
+    for category in categorical :
+        for partition in working_partitions:
+            for cat_type in df.loc[partition,category] :
+                if df.loc[partition, category].value_counts().min() <k  :
+                    print("\nKanonimity cannot be applied on all columns so merging ",category)
+                    print("Splitting the actual data set again")
+                    merged_categories.append(category)
+                    categorical.remove(category)
+                    return partition_dataset(df_copy,feature_columns,categorical,numerical,scale,sensitive_column,k,l_div)
+               
+    
+    
 
     return working_partitions
 
@@ -100,6 +115,8 @@ def get_anonymize_dataset(df,finished_partitions,feature_columns,numerical):
             max_value = rows[column].max()
             min_value = rows[column].min()
             rows[column]="["+str(min_value)+" - "+str(max_value)+"]"
+        for category in merged_categories :
+            rows[category] = '*'
         new_df = pd.DataFrame(columns=df.columns)
         new_df = pd.concat([new_df, rows],ignore_index=True)
         final_df = new_df.filter(feature_columns, axis=1)
