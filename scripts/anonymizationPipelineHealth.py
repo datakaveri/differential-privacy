84423472179393ac9d3514b3e22d90883b14bdd8
novoneel-iudx <novoneel.chakraborty@datakaveri.org>
2024-03-21 16:31:57 +0530

    updating repository to remove remnants from other branches and keep only requirements for health data anonyizer

--- scripts/anonymizationPipelineHealth.py
+++ scripts/anonymizationPipelineHealth.py
@@ -1,89 +1,26 @@
-# import statements
-import pandas as pd
-import numpy as np
-import json
-import hashlib
+# script for function call handling 
+import anonymizationPipelineHealthModules as mods
 
-# function definitions
+# necessary file reads
+config = mods.read_config("pipelineConfig.json")
+data = mods.read_data(config["data_file"])
 
-# reading config
-def read_config(configFile):
-    with open(configFile, "r") as cfile:
-        config = json.load(cfile)
-    return config
+# checking the order of operations required
+operations = mods.oop_handler(config)
+print(operations)
 
-# reading data
-def read_data(dataFile):
-    with open(dataFile, "r") as dfile:
-        data = json.load(dfile)
-        dataframe = pd.json(data)
-    return dataframe
+if "suppress" in operations:
+    print("Performing Attribute Suppression")
+    data = mods.suppress(data, config)
+if "pseudonymize" in operations:
+    print("Performing Attribute Pseudonymization: Pseudonymized Attribute stored in Hashed Value Column")
+    data = mods.pseudonymize(data, config)
+# if "k_anonymize" in operations:
+#     print("K-Anonymizing Data")
+#     data = mods.k_anonymize(data, config)
+if "dp" in operations:
+    print("Enacting Differential Privacy")
+    data = mods.differential_privacy(data, config)
 
-# suppress
-def suppress(dataframe, config):
-    attributes_to_suppress = config['suppress']
-    dataframe.drop(columns = suppress, inplace = True)
-    print(dataframe.info())
-    return dataframe
-
-# pseudonymize
-def pseudonymize(dataframe, config):
-    attribute_to_pseudonymize = config['pseudonymize']
-    dataframe['Hashed Value'] = dataframe[attribute_to_pseudonymize].apply(lambda x:hashlib.sha256(x.encode()).hexdigest())
-    dataframe.drop(columns = attribute_to_pseudonymize, inplace = True)
-    return dataframe
-
-###########################
-# functions for k-anonymity
-
-# function to bin the ages
-def generalize_age(dataframe, bins):
-    dataframe['Age Bin'], retbins = pd.cut(dataframe['Age'], bins, retbins = True, ordered = True)
-    return dataframe, retbins
-
-# function to check if a bin violates k-anonymity
-def violation_checker(k, data):
-    age_value_counts = data['Age Bin'].value_counts()
-    violating_bins = age_value_counts[(age_value_counts < k) & (age_value_counts > 0)].index
-    # print(violating_bins)
-    if len(violating_bins) > 0:
-        return True
-    else:
-        return False
-
-# function to k-anonymize
-def k_anonymize(dataframe, config, age_bins):
-    k = config["k"]
-    min_bin_value = config["min_bin_value"]
-    max_bin_value = config["max_bin_value"]
-    # age_bins = np.arange(min_bin_value, max_bin_value, 1)
-    # If ANY bin violates k-anonymity, increment the size of ALL bins
-    for i in range(1, (len(age_bins) - 1)):
-        while violation_checker(k, data, bins) == True:
-            data, bins = generalize_age(data, np.arange(0,101,i))
-            age_value_counts = data['Age Bin'].value_counts()
-            print(age_value_counts) 
-            i+=1
-    return dataframe
-
-###########################
-# function to implement DP
-    # query: number of users for a diagnosis
-    # neighbouring dataset: add or remove a user from original dataset
-    # sensitivity: 1
-
-def query_building(dataframe, config):
-    query_column = config["dp_query"]
-    aggregation_column = config["dp_aggregation"]
-    dataframe = dataframe.groupby(query_column).agg({aggregation_column:"count"})
-    dataframe.rename(columns={aggregation_column:"Count"}, inplace = True)
-    return dataframe
-
-def differential_privacy(dataframe, config):
-    epsilon = config["dp_epsilon"]
-    sensitivity = 1
-    b = sensitivity/epsilon
-    noise = np.random.laplace(0, b, len(dataframe))
-    dataframe = dataframe["Count"] + noise
-    dataframe.rename(columns={"Count":"Noisy Count"})
-    return dataframe
+print(data)
+print(data.head())
---
+++ scripts/anonymizationPipelineHealthModules.py
@@ -0,0 +1,111 @@
+# import statements
+import pandas as pd
+import numpy as np
+import json
+import hashlib
+
+###########################
+# function definitions
+
+# read config
+def read_config(configFile):
+    with open(configFile, "r") as cfile:
+        config = json.load(cfile)
+    return config
+
+# read data
+def read_data(dataFile):
+    with open(dataFile, "r") as dfile:
+        data = json.load(dfile)
+        dataframe = pd.json_normalize(data)
+    return dataframe
+
+# suppress
+def suppress(dataframe, config):
+    attributes_to_suppress = config['suppress']
+    dataframe.drop(columns = attributes_to_suppress, inplace = True)
+    print(dataframe.info())
+    return dataframe
+
+# pseudonymize
+def pseudonymize(dataframe, config):
+    attribute_to_pseudonymize = config['pseudonymize']
+    dataframe['Hashed Value'] = dataframe[attribute_to_pseudonymize].apply(lambda x:hashlib.sha256(x.encode()).hexdigest())
+    dataframe.drop(columns = attribute_to_pseudonymize, inplace = True)
+    return dataframe
+
+###########################
+# functions for k-anonymity
+
+# function to bin the ages
+def generalize(dataframe, config, bins):
+    attribute_to_generalize = config["generalize"]
+    dataframe['Age Bin'] = pd.cut(dataframe[attribute_to_generalize], bins, ordered = True)
+    return dataframe
+
+# function to check if a bin violates k-anonymity
+def violation_checker(k, data):
+    age_value_counts = data['Age Bin'].value_counts()
+    violating_bins = age_value_counts[(age_value_counts < k) & (age_value_counts > 0)].index
+    # print(violating_bins)
+    if len(violating_bins) > 0:
+        return True
+    else:
+        return False
+
+# function to k-anonymize
+def k_anonymize(dataframe, config):
+    attribute_to_generalize = config["generalize"]
+    k = config["k"]
+    min_bin_value = config["min_bin_value"]
+    max_bin_value = config["max_bin_value"]
+    age_bins = np.arange(min_bin_value, max_bin_value, 1)
+    dataframe = generalize(dataframe, config, np.arange(min_bin_value,max_bin_value,1))
+    # If ANY bin violates k-anonymity, increment the size of ALL bins
+    for i in range(1, (len(age_bins) - 1)):
+        while violation_checker(k, dataframe) == True:
+            dataframe = generalize(dataframe, config, np.arange(min_bin_value,max_bin_value,i))
+            age_value_counts = dataframe['Age Bin'].value_counts()
+            print(age_value_counts) 
+            i+=1
+    dataframe.drop(columns = attribute_to_generalize, inplace = True)
+    return dataframe
+
+###########################
+# function to implement DP
+    # query: number of users for a diagnosis
+    # neighbouring dataset: add or remove a user from original dataset
+    # sensitivity: 1
+
+def query_building(dataframe, config):
+    query_column = config["dp_query"]
+    aggregation_column = config["dp_aggregation"]
+    dataframe = dataframe.groupby(query_column).agg({aggregation_column:"count"})
+    dataframe.rename(columns={aggregation_column:"Count"}, inplace = True)
+    return dataframe
+
+def differential_privacy(dataframe, config):
+    dataframe = query_building(dataframe, config)
+    epsilon = config["dp_epsilon"]
+    sensitivity = 1
+    b = sensitivity/epsilon
+    noise = np.random.laplace(0, b, len(dataframe))
+    dataframe["Noisy Count"] = dataframe["Count"] + noise
+    dataframe["Noisy Count"].clip(0, np.inf, inplace = True)
+    dataframe["Noisy Count"] = dataframe["Noisy Count"].round().astype(int)
+    dataframe.drop(columns = "Count", inplace = True)
+    return dataframe
+
+###########################
+# function to handle order of operations
+def oop_handler(config):
+    operations = []
+    if "suppress" in config:
+        operations.append("suppress")
+    if "pseudonymize" in config:
+        operations.append("pseudonymize")
+    if "generalize" in config:
+        operations.append("k_anonymize")
+    if "dp_aggregation" in config:
+        operations.append("dp")
+    return operations
